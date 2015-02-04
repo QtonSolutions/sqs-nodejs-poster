7a459aaeddb8542f0a5f05390720426257c9c3fc
DenisH <denish@isocra.com>
2015-02-04 15:38:36 +0000

    Code for testing posting a message to an SQS queue

---
+++ server.js
@@ -0,0 +1,75 @@
+var should = AWS = require('aws-sdk'),
+ awsRegion = 'eu-west-1',
+ sqs = {},
+ Hapi = require('hapi');
+
+// Read the credentials from ~/.aws/credentials
+var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+AWS.config.credentials = credentials;
+
+var server = new Hapi.Server();
+server.connection({ port: process.env.PORT || 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+function sendSqsMessage(param) {
+    'use strict';
+
+    AWS.config.update({region: awsRegion});
+    sqs = new AWS.SQS();
+
+    var params = {
+        MessageBody: param,
+        QueueUrl: 'https://sqs.eu-west-1.amazonaws.com/929063684942/job-test-to-denis',
+        DelaySeconds: 0
+    };
+
+    sqs.sendMessage(params, function(err) {
+        if (err) {
+            console.log(err, err.stack);
+        } // an error occurred
+        else {
+            console.log('Victory, message sent for ' + param + '!');
+        }
+    });
+}
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        var param = encodeURIComponent(request.params.name);
+        sendSqsMessage(param);
+        reply('Your message ' + param + ' has been sent to queue!');
+    }
+});
+
+var options = {
+    opsInterval: 1000,
+    reporters: [{
+        reporter: require('good-console'),
+        args:[{ log: '*', response: '*' }]
+    }]
+};
+
+server.register({
+    register: require('good'),
+    options: options
+}, function (err) {
+
+    if (err) {
+        console.error(err);
+    }
+    else {
+        server.start(function () {
+
+            console.info('Server started at ' + server.info.uri);
+        });
+    }
+});
---
+++ test/aws_sdk.js
@@ -0,0 +1,32 @@
+var should = require ('should');
+var AWS = require('aws-sdk');
+
+var awsRegion = 'eu-west-1';
+
+describe ( 'When trying out this sample application in AWS you', function () {
+ 
+  before(function () {
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      region: awsRegion});
+  });
+
+  it ( 'should have an environment variable set for AWS_ACCESS_KEY_ID', function () {
+    process.env.AWS_ACCESS_KEY_ID.should.exist;
+  });
+ 
+  it ( 'should have an environment variables set for AWS_SECRET_KEY', function () {
+    process.env.AWS_SECRET_KEY.should.exist;
+  });
+
+  it('should have the AWS Access Key set in the AWS config', function () {
+    var config = AWS.config;
+    config.credentials.accessKeyId.should.equal(process.env.AWS_ACCESS_KEY_ID);
+    config.credentials.secretAccessKey.should.equal(process.env.AWS_SECRET_KEY);
+  });
+
+  it('should have the AWS region set to us west 2', function () {
+    AWS.config.region.should.equal(awsRegion);
+  });
+});
